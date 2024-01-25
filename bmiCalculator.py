bebcb4840ce3daff745239bfef2d2e7d3aa1f9b1
RobertSajdak <robert.sajdak@onet.eu>
2024-01-25 13:57:16 +0100

    Calculate_BMI function added.

--- bmiCalculator.py
+++ bmiCalculator.py
@@ -1,5 +1,16 @@
 import tkinter as tk
 
+
+def calculate_bmi():
+    try:
+        weight = float(weight_entry.get())
+        height = float(height_entry.get())
+        bmi = weight / (height * height)
+        result_label.config(text=f"Your BMI index: {bmi: .2f}")
+    except ValueError:
+        result_label.config(text="Please, enter valid weight and height.")
+
+
 # Create the main window.
 window = tk.Tk()
 window.title("BMI Calculator")
@@ -15,6 +26,14 @@ height_label = tk.Label(window, text="Height [m]:")
 height_label.pack(padx=10, pady=5, fill=tk.BOTH, expand=True)
 height_entry = tk.Entry(window)
 height_entry.pack(padx=10, pady=5, fill=tk.BOTH, expand=True)
+
+# Create a button to calculate the BMI.
+calculate_button = tk.Button(window, text="Calculate BMI", command=calculate_bmi)
+calculate_button.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+# Create a label to display the BMI result.
+result_label = tk.Label(window, text="")
+result_label.pack(padx=10, pady=10, fill=tk. BOTH, expand=True)
 
 # Start the main loop.
 window.mainloop()
